7e339e1bc2f132fe8de11bbd8ee011ee26de6d46
usingh2 <usingh2@usingh2.myguest.virtualbox.org>
2023-04-02 00:21:39 -0400

    V3.1.0
    Major fixes in CMakeLists, major fix in CMake command

--- moving.c
+++ moving.c
@@ -1,96 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>	//to use system()
 #include <string.h>	
-#include <dirent.h>
+//#include <dirent.h>
 
-#define BUILD_COUNTER_FILE "build_counter.txt"
-#define MAX_COUNT 2
+// #define BUILD_COUNTER_FILE "build_counter.txt"
+// #define MAX_COUNT 2
 
-int increment_build_counter() {
-    FILE* file;
-    int count = 0;
+// int increment_build_counter() {
+//     FILE* file;
+//     int count = 0;
 
-    // Open the file for reading
-    file = fopen(BUILD_COUNTER_FILE, "r");
-    if (file != NULL) {
-        // Read the current count from the file
-        fscanf(file, "%d", &count);
-        fclose(file);
-    }
+//     // Open the file for reading
+//     file = fopen(BUILD_COUNTER_FILE, "r");
+//     if (file != NULL) {
+//         // Read the current count from the file
+//         fscanf(file, "%d", &count);
+//         fclose(file);
+//     }
 
-    // Open the file for writing and increment the count
-    file = fopen(BUILD_COUNTER_FILE, "w");
-    if (file != NULL) {
-        fprintf(file, "%d", count + 1);
-        fclose(file);
-    }
+//     // Open the file for writing and increment the count
+//     file = fopen(BUILD_COUNTER_FILE, "w");
+//     if (file != NULL) {
+//         fprintf(file, "%d", count + 1);
+//         fclose(file);
+//     }
     
-    return count;
-}
+//     return count;
+// }
 
-char* diff_files() {
-    char command[100];
-    char buffer[256];
-    char* result = NULL;
-    FILE* pipe;
+// char* diff_files() {
+//     char command[100];
+//     char buffer[256];
+//     char* result = NULL;
+//     FILE* pipe;
 
-    // Build the command string with hardcoded file names
-    sprintf(command, "diff file1.txt file2.txt");
+//     // Build the command string with hardcoded file names
+//     sprintf(command, "diff file1.txt file2.txt");
 
-    // Open the command as a pipe and read the output
-    pipe = popen(command, "r");
-    if (!pipe) {
-        printf("Error executing command\n");
-        return NULL;
-    }
+//     // Open the command as a pipe and read the output
+//     pipe = popen(command, "r");
+//     if (!pipe) {
+//         printf("Error executing command\n");
+//         return NULL;
+//     }
 
-    while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
-        // Allocate memory for the result string
-        if (result == NULL) {
-            result = (char*)malloc(strlen(buffer) + 1);
-            strcpy(result, buffer);
-        }
-        else {
-            // Expand the result string
-            result = (char*)realloc(result, strlen(result) + strlen(buffer) + 1);
-            strcat(result, buffer);
-        }
-    }
+//     while (fgets(buffer, sizeof(buffer), pipe) != NULL) {
+//         // Allocate memory for the result string
+//         if (result == NULL) {
+//             result = (char*)malloc(strlen(buffer) + 1);
+//             strcpy(result, buffer);
+//         }
+//         else {
+//             // Expand the result string
+//             result = (char*)realloc(result, strlen(result) + strlen(buffer) + 1);
+//             strcat(result, buffer);
+//         }
+//     }
 
-    // Close the pipe and print the status
-    int status = pclose(pipe);
-    if (status == -1) {
-        printf("Error closing pipe\n");
-    }
-    else {
-        printf("Command exited with status %d\n", status);
-    }
+//     // Close the pipe and print the status
+//     int status = pclose(pipe);
+//     if (status == -1) {
+//         printf("Error closing pipe\n");
+//     }
+//     else {
+//         printf("Command exited with status %d\n", status);
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
 int main(int argc, char *argv[])
 {
 	char command[100]="cp -avr /data/data/";
 	char end_command[50]=" /sdcard/";
 	
-	//Create two files to keep track
-	char new_list_app[50]="touch new_list.txt";
+	// //Create two files to keep track
+	// char new_list_app[50]="touch new_list.txt";
 	
-	int count = increment_build_counter();
+	// int count = increment_build_counter();
 	
-	if (count < 2) {
-		char old_list_app[50]="touch old_list.txt";
-		system(old_list_app);
-		}
+	// if (count < 2) {
+	// 	char old_list_app[50]="touch old_list.txt";
+	// 	system(old_list_app);
+	// 	}
 	
 	//TEMP: concatenating which files to stage
 	strcat(command,argv[1]);
-    	strcat(command,end_command);
+    strcat(command,end_command);
 	system(command);
 	
 	//execute the new file
-	system(new_list_app);
+	// system(new_list_app);
 	
 	return 0;
 }
--- pull_database.py
+++ pull_database.py
@@ -2,56 +2,61 @@ import subprocess
 import os
 
 
-while True:
-	
-	package_name=input("Enter the package name: ") 
-	
-	#to get the current working directory
-	python_directory_path = os.getcwd()
-	
-	#Android PAths
-	data_lcl_tmp_path="/data/local/tmp/"
-	sdcard_path="/sdcard/{pack_name}".format(pack_name=package_name)
-	
-	#Change the following as necessary
-	desktop_path="/home/usingh_desk/Desktop/"
-	path_to_NDK_cmake="/home/usingh_desk/Desktop/LM/android-ndk-r25c/build/cmake/android.toolchain.cmake"
-	c_code_path=python_directory_path
-	
-	#executable name
-	ndk_executable="copydbF"
+#to get the current working directory
+python_directory_path = os.getcwd()
 
-	cmd="cmake {path_NDK} -DCMAKE_TOOLCHAIN_FILE= -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=16 {c_code}".format(path_NDK=path_to_NDK_cmake,c_code=c_code_path)
-	subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+#Name of the current user
+cmd=subprocess.run("whoami",stdout=subprocess.PIPE)
+user_name=cmd.stdout.decode("utf-8").strip()
 
-	cmd="cd {curnt_path} && make".format(curnt_path=python_directory_path)
-	subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+#Change the following as necessary
+desktop_path="/home/{user}/Desktop/".format(user=user_name)
+path_to_NDK_cmake="{desktop_p}/LM/android-ndk-r25c/build/cmake/android.toolchain.cmake".format(desktop_p=desktop_path)
+current_dir=python_directory_path
 
-	cmd="adb push {curnt_path}/{exe} /sdcard/".format(curnt_path=python_directory_path,and_path=data_lcl_tmp_path,exe=ndk_executable)
-	subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+#executable name
+ndk_executable="copydbF"
 
-	cmd="adb shell \"su -c \'mv /sdcard/copydbF /data/local/tmp\'\""
-	subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+# while True:
+package_name=input("Enter the package name: ") 
 
-	cmd="adb shell \"su -c \'chmod 777 {and_path}{exe}\'\"".format(and_path=data_lcl_tmp_path,exe=ndk_executable)
-	subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+#Android Paths
+data_lcl_tmp_path="/data/local/tmp/"
+sdcard_path="/sdcard/{pack_name}".format(pack_name=package_name)
 
-	cmd="adb shell \"su -c \'{and_path}{exe} {arg_package}\' 2>/dev/null\"".format(and_path=data_lcl_tmp_path,arg_package=package_name,exe=ndk_executable)
-	subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+cmd="cmake -DCMAKE_TOOLCHAIN_FILE={path_NDK} -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=16 {cur_dir}".format(path_NDK=path_to_NDK_cmake,cur_dir=current_dir)
+subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
 
-	cmd="adb pull {sdcard} {desk}".format(sdcard=sdcard_path,desk=desktop_path)
-	subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
-	
-	print()
-	print(50*("*"))
-	print(50*("*"))
-	decision=input("Would you like to search more apps (y/n): ")
-	print(50*("*"))
-	print(50*("*"))
-	print()
-	
-	if decision=='n':
-		break
+cmd="cd {curnt_path} && make".format(curnt_path=python_directory_path)
+subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+
+cmd="adb push {curnt_path}/{exe} /sdcard/".format(curnt_path=python_directory_path,and_path=data_lcl_tmp_path,exe=ndk_executable)
+subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+
+#Change
+cmd="adb shell \"su -c \'mv /sdcard/{ndk_exe} {lcl_tmp}\'\"".format(ndk_exe=ndk_executable,lcl_tmp=data_lcl_tmp_path)
+subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+
+cmd="adb shell \"su -c \'chmod 777 {and_path}{exe}\'\"".format(and_path=data_lcl_tmp_path,exe=ndk_executable)
+subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+
+
+cmd="adb shell \"su -c \'{and_path}{exe} {arg_package}\' 2>/dev/null\"".format(and_path=data_lcl_tmp_path,arg_package=package_name,exe=ndk_executable)
+subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+
+cmd="adb pull {sdcard} {desk}".format(sdcard=sdcard_path,desk=desktop_path)
+subprocess.run(cmd,stdout=subprocess.PIPE,shell=True)
+
+# print()
+# print(50*("*"))
+# print(50*("*"))
+# decision=input("Would you like to search more apps (y/n): ")
+# print(50*("*"))
+# print(50*("*"))
+# print()
+
+# if decision=='n':
+# 	break
 
 
 #Test: 
