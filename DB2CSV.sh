2b8235b9afe473539f225757239c313f74b39334
Sruthi Chavali <sruthichavali@hotmail.com>
2023-04-13 14:58:07 -0400

    if statement in db to csv file

--- DB2CSV.sh
+++ DB2CSV.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#sqlite3 database.sqlite <<EOF  #signal
-#.headers on
-#.mode csv
-#.output signalMessagesDB.csv
-#SELECT * FROM message
-#EOF
 
+if [ -e /home/kali/Desktop/Imports/Signal/database.sqlite ]; then
+	sqlite3 database.sqlite <<EOF  #signal
+	.headers on
+	.mode csv
+	.output signalMessagesDB.csv
+	SELECT * FROM message
+EOF
+fi
 
 sqlite3 msgstore.db <<EOF # whatsapp
 .headers on
